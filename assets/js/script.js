addf7bdccc72adb6b0cfc87cb053e9706fe43dd8
Julian Tuazon <jthrow00@gmail.com>
2020-01-21 14:27:40 -0800

    Added card flip feature and moved CSS files to CSS folder

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
+document.getElementById("gameCards").addEventListener('click', handleClick);
 
+function handleClick(event) {
+  if (event.target.className.indexOf("card-back") === -1) {
+    return;
+  }
+  console.log(event);
+  event.target.className += " hidden";
+}
