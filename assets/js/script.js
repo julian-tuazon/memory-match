c3023d943a461e53cd9a3bc6d7c3bf5e2177f4de
Julian Tuazon <jthrow00@gmail.com>
2020-01-24 03:20:14 -0800

    Modified styling of modals, added hover sounds

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-let firstGame = true;
+let firstGame = false; // Turned music auto-start for now
 let firstCardClicked;
 let secondCardClicked;
 let firstCardClasses;
@@ -9,6 +9,7 @@ let attempts = 0;
 let gamesPlayed = 0;
 let shuffleArray = [];
 let timeLeft = 10;
+let timer;
 let difficulty;
 
 
@@ -46,6 +47,7 @@ const onSound = document.getElementById("on-sound");
 const offSound = document.getElementById("off-sound");
 const resetSound = document.getElementById("reset-sound");
 const cheatSound = document.getElementById("cheat-sound");
+const hoverSound = document.getElementById("hover-sound");
 const soundEffectsArray = [
   startSound,
   endSound,
@@ -56,7 +58,6 @@ const soundEffectsArray = [
   offSound,
   resetSound,
   cheatSound,
-  qirex,
 ];
 const voiceArray = [
   "ag",
@@ -69,6 +70,7 @@ const voiceArray = [
   "qirex",
   "triakis",
 ];
+var hoverableList = document.getElementsByClassName("card-back");
 
 // Closure for the toggleMusic functionality attached to the musicButton/gamesPlayedDisplay
 let toggleMusic = (function() {
@@ -132,6 +134,94 @@ let toggleVoice = (function() {
 const voiceButton = matchesDisplay;
 voiceButton.addEventListener('click', toggleVoice);
 
+// Adding hover effects to cards and other desired elements
+// let toggleHover = (function() {
+//   let toggle = false;
+//   for (let i = 0; i < hoverableList.length; i++) {
+//     hoverableList[i].addEventListener('mouseenter', function(){
+//       hoverSound.play();
+//     });
+//   }
+//   return function () {
+//     if (toggle) {
+//       onSound.play();
+//       toggle = false;
+//     } else {
+//       offSound.play();
+//       toggle = true;
+//     }
+//     hoverSound.muted = toggle;
+//   }
+// })();
+
+// for (let i = 0; i < hoverableList.length; i++) {
+//   hoverableList[i].addEventListener('mouseenter', function () {
+//     hoverSound.play();
+//   })
+// }
+
+var clickableList = document.getElementsByClassName("clickable");
+for (let x = 0; x < clickableList.length; x++) {
+  clickableList[x].addEventListener('mouseover', function() {
+    console.log("mouseenter clickable");
+    hoverSound.play();
+  })
+}
+// Does not work
+for (let y = 0; y < hoverableList.length; y++) {
+  hoverableList[y].addEventListener('mouseover', function () {
+    console.log("mouseenter clickable");
+    hoverSound.play();
+  })
+}
+
+
+document.getElementsByClassName("nine-s")[0].addEventListener('mouseover', function() {
+  console.log('mouseover 9S');
+  document.getElementById("welcome-message2").textContent = "YoRHa No. 9 Type S";
+  document.getElementById("welcome-message").textContent = "E A S Y";
+  hoverSound.play();
+});
+
+document.getElementsByClassName("nine-s")[0].addEventListener('mouseleave', function () {
+  console.log('mouseexit 9S');
+  document.getElementById("welcome-message2").textContent = "//";
+  document.getElementById("welcome-message").textContent = "m e m o r y . e x e";
+});
+
+document.getElementsByClassName("nine-s")[0].addEventListener('click', function () {
+  console.log('click 9S');
+
+  hoverSound.play();
+});
+
+document.getElementsByClassName("a-two")[0].addEventListener('mouseover', function () {
+  console.log('mouseover 9S');
+  document.getElementById("welcome-message2").textContent = "YoRHa No. 2 Type A";
+  document.getElementById("welcome-message").textContent = "M E D I U M";
+  hoverSound.play();
+});
+
+document.getElementsByClassName("a-two")[0].addEventListener('mouseleave', function () {
+  console.log('mouseleave a2');
+  document.getElementById("welcome-message2").textContent = "//";
+  document.getElementById("welcome-message").textContent = "m e m o r y . e x e";
+});
+
+document.getElementsByClassName("two-b")[0].addEventListener('mouseover', function () {
+  console.log('mouseover 9S');
+  document.getElementById("welcome-message2").textContent = "YoRHa No. 2 Type B";
+  document.getElementById("welcome-message").textContent = "H A R D";
+  hoverSound.play();
+});
+
+document.getElementsByClassName("two-b")[0].addEventListener('mouseleave', function () {
+  console.log('mouseleave 2b');
+  document.getElementById("welcome-message2").textContent = "//";
+  document.getElementById("welcome-message").textContent = "m e m o r y . e x e";
+});
+
+
 // General events
 
 function startGame() {
@@ -164,7 +254,7 @@ function handleClick(event) {
     gameCards.removeEventListener('click', handleClick);
     if (firstCardClasses == secondCardClasses) {
       correctSound.play();
-      // Gets the logo name of card class and plays audio with associated id
+      // Gets the logo name of card class and plays audio element with associated id
       // let endIndex = secondCardClasses.indexOf("-");
       let logoName = secondCardClasses.slice(11, secondCardClasses.indexOf("-logo"));
       document.getElementById(`${logoName}`).play();
@@ -180,11 +270,12 @@ function handleClick(event) {
         secondCardClicked = null;
         gameCards.classList.remove("correct");
         gameCards.addEventListener('click', handleClick);
-      }, 750);
+      }, 100); //previous: 750
       if (matches === maxMatches) {
         endSound.play();
         document.getElementById("final-accuracy").textContent = "Accuracy: " + accuracyDisplay.textContent;
         end.classList.remove("hidden");
+        welcome.classList.remove("hidden");
       }
     } else {
       incorrectSound.play();
@@ -200,7 +291,7 @@ function handleClick(event) {
         secondCardClicked = null;
         gameCards.classList.remove("incorrect");
         gameCards.addEventListener('click', handleClick);
-      }, 1250);
+      }, 100); //previous 1250
     }
   }
 }
@@ -240,11 +331,11 @@ function resetGame() {
   end.classList.add("hidden");
 }
 
-function timer() {
+function countdown() {
   if (timeLeft <= 0) {
     document.getElementById("end-message").textContent = "Defeat! ごめんなさい!";
     end.classList.remove("hidden");
-    end.classList.remove("hidden");
+    welcome.classList.remove("hidden");
   }
   timeLeft -= 0.1;
 }
