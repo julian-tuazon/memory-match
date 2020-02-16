b2f76681d9ee2fa4eaa41bf6e939ebbbbcea9b6a
Julian Tuazon <jthrow00@gmail.com>
2020-02-15 21:38:18 -0800

    Modified difficulty.js to have difficulty object that holds relevant time, lives, display text, etc; difficulty.js now contains event handlers for difficulty modal selection

--- assets/js/difficulty.js
+++ assets/js/difficulty.js
@@ -4,67 +4,82 @@ class Difficulty {
   }
 }
 
-const mode = {
+const difficulty = {
   "current": "null",
-  "defaultTitle": "M O D E",
-  "currentTitle": "M O D E",
-  "defaultMessage": "Deploy Pod",
-  "currentMessage": "Deploy Pod",
-  "modeList": [
-    "survival",
-    "time-attack",
+  "defaultTitle": "D I F F I C U L T Y",
+  "currentTitle": "D I F F I C U L T Y",
+  "defaultMessage": "Deploy YoRHa Unit",
+  "currentMessage": "Deploy YoRHa Unit",
+  "difficultyList": [
+    "easy",
+    "medium",
+    "hard",
   ],
-  "survival": {
-    "modeTitle": "S U R V I V A L",
-    "modeMessage": "Pod X-042",
-    "sound": document.getElementById("pod-one"),
+  "easy": {
+    "difficultyTitle": "E A S Y",
+    "difficultyMessage": "YoRHa No. 9 Type S",
+    "time": 99,
+    "lives": 50,
+    "display": "Easy",
+    "sound": document.getElementById("9s"),
   },
-  "time-attack": {
-    "modeTitle": "T I M E - A T T A C K",
-    "modeMessage": "Pod Y-153",
-    "sound": document.getElementById("pod-two"),
+  "medium": {
+    "difficultyTitle": "M E D I U M",
+    "difficultyMessage": "YoRHa Type A No. 2",
+    "time": 60,
+    "lives": 35,
+    "display": "Medium",
+    "sound": document.getElementById("a2"),
+  },
+  "hard": {
+    "difficultyTitle": "H A R D",
+    "difficultyMessage": "YoRHa No. 2 Type B",
+    "time": 30,
+    "lives": 20,
+    "display": "Hard",
+    "sound": document.getElementById("2b"),
   },
 };
 
-function addEventListenersMode() {
-  for (let i = 0; i < mode.modeList.length; i++) {
-    let currentElement = document.getElementById(`${mode.modeList[i]}`);
-    currentElement.addEventListener('mouseover', handleMouseOverMode);
-    currentElement.addEventListener('mouseleave', handleMouseLeaveMode);
+function addEventListenersDifficulty() {
+  for (let i = 0; i < difficulty.difficultyList.length; i++) {
+    let currentElement = document.getElementById(`${difficulty.difficultyList[i]}`);
+    currentElement.addEventListener('mouseover', handleMouseOverDifficulty);
+    currentElement.addEventListener('mouseleave', handleMouseLeaveDifficulty);
     currentElement.addEventListener('click', function (event) {
-      handleClickMode(event);
+      handleClickDifficulty(event);
     });
   }
 }
 
-function handleMouseOverMode(event) {
-  document.getElementById("mode-title").textContent = mode[event.target.id].modeTitle;
-  document.getElementById("mode-message").textContent = mode[event.target.id].modeMessage;
+function handleMouseOverDifficulty(event) {
+  document.getElementById("difficulty-title").textContent = difficulty[event.target.id].difficultyTitle;
+  document.getElementById("difficulty-message").textContent = difficulty[event.target.id].difficultyMessage;
 }
 
-function handleMouseLeaveMode() {
-  document.getElementById("mode-title").textContent = mode.currentTitle;
-  document.getElementById("mode-message").textContent = mode.currentMessage;
+function handleMouseLeaveDifficulty() {
+  document.getElementById("difficulty-title").textContent = difficulty.currentTitle;
+  document.getElementById("difficulty-message").textContent = difficulty.currentMessage;
 }
 
-function handleClickMode(event) {
-  if (mode.current !== event.target.id) {
+function handleClickDifficulty(event) {
+  if (difficulty.current !== event.target.id) {
     event.target.classList.add("selected", "selected-animation");
     event.target.classList.remove("clickable");
     setTimeout(function () {
       event.target.classList.remove("selected-animation");
     }, 700);
-    if (mode.current !== "null") {
-      document.getElementById(`${mode.current}`).classList.add("clickable");
-      document.getElementById(`${mode.current}`).classList.remove("selected");
+    if (difficulty.current !== "null") {
+      document.getElementById(`${difficulty.current}`).classList.add("clickable");
+      document.getElementById(`${difficulty.current}`).classList.remove("selected");
     }
-    mode.current = event.target.id;
-    mode.currentTitle = mode[mode.current].modeTitle;
-    mode.currentMessage = mode[mode.current].modeMessage;
+    difficulty.current = event.target.id;
+    difficulty.currentTitle = difficulty[difficulty.current].difficultyTitle;
+    difficulty.currentMessage = difficulty[difficulty.current].difficultyMessage;
     playSound(selectSound);
-    playSound(mode[mode.current].sound);
-    modeButton.classList.remove("temp-hidden");
+    playSound(difficulty[difficulty.current].sound);
+    difficultyButton.classList.remove("temp-hidden");
   }
 }
 
-addEventListenersMode();
+addEventListenersDifficulty();
