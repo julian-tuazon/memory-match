58beb1fa02225366313badcb5bacc52786fb7e26
Julian <jthrow00@gmail.com>
2020-03-31 13:38:09 -0700

    Modified difficulty module to only contain difficulty data object

--- assets/js/difficulty.js
+++ assets/js/difficulty.js
@@ -1,85 +1,38 @@
-class Difficulty {
-  constructor() {
-
-  }
-}
-
-const difficulty = {
+const myDifficulty = {
+  "name": "difficulty",
+  "button": document.getElementById('difficulty-button'),
   "current": "null",
   "defaultTitle": "D I F F I C U L T Y",
   "currentTitle": "D I F F I C U L T Y",
   "defaultMessage": "Deploy YoRHa Unit",
   "currentMessage": "Deploy YoRHa Unit",
-  "difficultyList": [
+  "itemList": [
     "easy",
     "medium",
     "hard",
   ],
   "easy": {
-    "difficultyTitle": "E A S Y",
-    "difficultyMessage": "YoRHa No. 9 Type S",
+    "title": "E A S Y",
+    "message": "YoRHa No. 9 Type S",
     "time": 99,
     "lives": 50,
     "display": "Easy",
-    "sound": document.getElementById("9s"),
+    "sound": document.getElementById("easy-voice"),
   },
   "medium": {
-    "difficultyTitle": "M E D I U M",
-    "difficultyMessage": "YoRHa Type A No. 2",
+    "title": "M E D I U M",
+    "message": "YoRHa Type A No. 2",
     "time": 60,
     "lives": 35,
     "display": "Medium",
-    "sound": document.getElementById("a2"),
+    "sound": document.getElementById("medium-voice"),
   },
   "hard": {
-    "difficultyTitle": "H A R D",
-    "difficultyMessage": "YoRHa No. 2 Type B",
-    "time": 30,
-    "lives": 20,
+    "title": "H A R D",
+    "message": "YoRHa No. 2 Type B",
+    "time": 3,
+    "lives": 2,
     "display": "Hard",
-    "sound": document.getElementById("2b"),
+    "sound": document.getElementById("hard-voice"),
   },
 };
-
-function addEventListenersDifficulty() {
-  for (let i = 0; i < difficulty.difficultyList.length; i++) {
-    let currentElement = document.getElementById(`${difficulty.difficultyList[i]}`);
-    currentElement.addEventListener('mouseover', handleMouseOverDifficulty);
-    currentElement.addEventListener('mouseleave', handleMouseLeaveDifficulty);
-    currentElement.addEventListener('click', function (event) {
-      handleClickDifficulty(event);
-    });
-  }
-}
-
-function handleMouseOverDifficulty(event) {
-  document.getElementById("difficulty-title").textContent = difficulty[event.target.id].difficultyTitle;
-  document.getElementById("difficulty-message").textContent = difficulty[event.target.id].difficultyMessage;
-}
-
-function handleMouseLeaveDifficulty() {
-  document.getElementById("difficulty-title").textContent = difficulty.currentTitle;
-  document.getElementById("difficulty-message").textContent = difficulty.currentMessage;
-}
-
-function handleClickDifficulty(event) {
-  if (difficulty.current !== event.target.id) {
-    event.target.classList.add("selected", "selected-animation");
-    event.target.classList.remove("clickable");
-    setTimeout(function () {
-      event.target.classList.remove("selected-animation");
-    }, 700);
-    if (difficulty.current !== "null") {
-      document.getElementById(`${difficulty.current}`).classList.add("clickable");
-      document.getElementById(`${difficulty.current}`).classList.remove("selected");
-    }
-    difficulty.current = event.target.id;
-    difficulty.currentTitle = difficulty[difficulty.current].difficultyTitle;
-    difficulty.currentMessage = difficulty[difficulty.current].difficultyMessage;
-    playSound(selectSound);
-    playSound(difficulty[difficulty.current].sound);
-    difficultyButton.classList.remove("temp-hidden");
-  }
-}
-
-addEventListenersDifficulty();
