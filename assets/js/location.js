4ee6044d2fecda65602674a872e4300c60183d01
Julian <jthrow00@gmail.com>
2020-03-31 13:51:58 -0700

    Created location module to hold location data object

---
+++ assets/js/location.js
@@ -0,0 +1,47 @@
+const myLocation = {
+  "name": "location",
+  "button": document.getElementById('location-button'),
+  "current": "null",
+  "defaultTitle": "L O C A T I O N",
+  "currentTitle": "L O C A T I O N",
+  "defaultMessage": "Select Location",
+  "currentMessage": "Select Location",
+  "itemList": [
+    "sol-2",
+    "moa-therma",
+    "vineta-k",
+    "tech-de-ra",
+    "metropia",
+    "anulpha-pass",
+  ],
+  "sol-2": {
+    "title": "L O C A T I O N",
+    "message": "Sol 2",
+    "sound": document.getElementById("sol-2-voice"),
+  },
+  "moa-therma": {
+    "title": "L O C A T I O N",
+    "message": "Moa Therma",
+    "sound": document.getElementById("moa-therma-voice"),
+  },
+  "vineta-k": {
+    "title": "L O C A T I O N",
+    "message": "Vineta K",
+    "sound": document.getElementById("vineta-k-voice"),
+  },
+  "tech-de-ra": {
+    "title": "L O C A T I O N",
+    "message": "Tech De Ra",
+    "sound": document.getElementById("tech-de-ra-voice"),
+  },
+  "metropia": {
+    "title": "L O C A T I O N",
+    "message": "Metropia",
+    "sound": document.getElementById("metropia-voice"),
+  },
+  "anulpha-pass": {
+    "title": "L O C A T I O N",
+    "message": "Anulpha Pass",
+    "sound": document.getElementById("anulpha-pass-voice"),
+  },
+};
