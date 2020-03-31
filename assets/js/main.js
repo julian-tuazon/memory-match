1cb097db1fd8259445fd0179981b4950e16bedf4
Julian <jthrow00@gmail.com>
2020-03-31 13:53:20 -0700

    Added main.js module to hold the main code and objects instantiated from classes

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+import { mode } from './mode';
+import { difficulty } from './difficulty';
+import { location } from './location';
+import Option from './option';
+
+const modeSelect = new Option(mode);
+const difficultySelect = new Option(difficulty);
+const locationSelect = new Option(location);
