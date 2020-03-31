1575523d9050cc8658249f81308b847f66857650
Julian <jthrow00@gmail.com>
2020-03-31 13:51:34 -0700

    Modified mode module to only contain mode data object

--- assets/js/mode.js
+++ assets/js/mode.js
@@ -1,72 +1,25 @@
-class Mode {
-  constructor() {
-
-  }
-}
-
-const mode = {
+const myMode = {
+  "name": "mode",
+  "button": document.getElementById('mode-button'),
   "current": "null",
   "defaultTitle": "M O D E",
   "currentTitle": "M O D E",
   "defaultMessage": "Deploy Pod",
   "currentMessage": "Deploy Pod",
-  "modeList": [
+  "itemList": [
     "survival",
     "time-attack",
   ],
   "survival": {
-    "modeTitle": "S U R V I V A L",
-    "modeMessage": "Pod X-042",
+    "title": "S U R V I V A L",
+    "message": "Pod X-042",
     "display": "Survival",
-    "sound": document.getElementById("pod-one"),
+    "sound": document.getElementById("survival-voice"),
   },
   "time-attack": {
-    "modeTitle": "T I M E - A T T A C K",
-    "modeMessage": "Pod Y-153",
+    "title": "T I M E - A T T A C K",
+    "message": "Pod Y-153",
     "display": "Time-Attack",
-    "sound": document.getElementById("pod-two"),
+    "sound": document.getElementById("time-attack-voice"),
   },
 };
-
-function addEventListenersMode() {
-  for (let i = 0; i < mode.modeList.length; i++) {
-    let currentElement = document.getElementById(`${mode.modeList[i]}`);
-    currentElement.addEventListener('mouseover', handleMouseOverMode);
-    currentElement.addEventListener('mouseleave', handleMouseLeaveMode);
-    currentElement.addEventListener('click', function (event) {
-      handleClickMode(event);
-    });
-  }
-}
-
-function handleMouseOverMode(event) {
-  document.getElementById("mode-title").textContent = mode[event.target.id].modeTitle;
-  document.getElementById("mode-message").textContent = mode[event.target.id].modeMessage;
-}
-
-function handleMouseLeaveMode() {
-  document.getElementById("mode-title").textContent = mode.currentTitle;
-  document.getElementById("mode-message").textContent = mode.currentMessage;
-}
-
-function handleClickMode(event) {
-  if (mode.current !== event.target.id) {
-    event.target.classList.add("selected", "selected-animation");
-    event.target.classList.remove("clickable");
-    setTimeout(function () {
-      event.target.classList.remove("selected-animation");
-    }, 700);
-    if (mode.current !== "null") {
-      document.getElementById(`${mode.current}`).classList.add("clickable");
-      document.getElementById(`${mode.current}`).classList.remove("selected");
-    }
-    mode.current = event.target.id;
-    mode.currentTitle = mode[mode.current].modeTitle;
-    mode.currentMessage = mode[mode.current].modeMessage;
-    playSound(selectSound);
-    playSound(mode[mode.current].sound);
-    modeButton.classList.remove("temp-hidden");
-  }
-}
-
-addEventListenersMode();
