1cbad5bf4f143f34943754fc84cc08368144a778
Julian <jthrow00@gmail.com>
2020-04-04 21:24:53 -0700

    Created End class with method setDisplay to update end modal display

---
+++ assets/js/end.js
@@ -0,0 +1,10 @@
+class End {
+  constructor() {
+  }
+
+  setDisplay(message, accuracy, timeLives) {
+    document.getElementById("end-message").textContent = message;
+    document.getElementById("end-accuracy").textContent = accuracy;
+    document.getElementById("end-time-lives").textContent = timeLives;
+  }
+}
