360f7c9e778d9e9000d85aa8535b499a7621f1c2
Julian <jthrow00@gmail.com>
2020-04-06 03:17:55 -0700

    Created Difficulty class with properties baseed on former difficulty obj

--- assets/js/difficulty.js
+++ assets/js/difficulty.js
@@ -1,38 +1,40 @@
-const myDifficulty = {
-  "name": "difficulty",
-  "button": document.getElementById('difficulty-button'),
-  "current": "null",
-  "defaultTitle": "D I F F I C U L T Y",
-  "currentTitle": "D I F F I C U L T Y",
-  "defaultMessage": "Deploy YoRHa Unit",
-  "currentMessage": "Deploy YoRHa Unit",
-  "itemList": [
-    "easy",
-    "medium",
-    "hard",
-  ],
-  "easy": {
-    "title": "E A S Y",
-    "message": "YoRHa No. 9 Type S",
-    "time": 99,
-    "lives": 50,
-    "display": "Easy",
-    "sound": document.getElementById("easy-voice"),
-  },
-  "medium": {
-    "title": "M E D I U M",
-    "message": "YoRHa Type A No. 2",
-    "time": 60,
-    "lives": 35,
-    "display": "Medium",
-    "sound": document.getElementById("medium-voice"),
-  },
-  "hard": {
-    "title": "H A R D",
-    "message": "YoRHa No. 2 Type B",
-    "time": 3,
-    "lives": 2,
-    "display": "Hard",
-    "sound": document.getElementById("hard-voice"),
-  },
-};
+class Difficulty {
+  constructor() {
+    this.name = "difficulty";
+    this.button = document.getElementById('difficulty-button');
+    this.current = "null";
+    this.defaultTitle = "D I F F I C U L T Y";
+    this.currentTitle = "D I F F I C U L T Y";
+    this.defaultMessage = "Deploy YoRHa Unit";
+    this.currentMessage = "Deploy YoRHa Unit";
+    this.itemList = [
+      "easy",
+      "medium",
+      "hard",
+    ];
+    this.easy = {
+      "title": "E A S Y",
+      "message": "YoRHa No. 9 Type S",
+      "time": 99,
+      "lives": 50,
+      "display": "Easy",
+      "sound": document.getElementById("easy-voice"),
+    };
+    this.medium = {
+      "title": "M E D I U M",
+      "message": "YoRHa Type A No. 2",
+      "time": 60,
+      "lives": 35,
+      "display": "Medium",
+      "sound": document.getElementById("medium-voice"),
+    };
+    this.hard = {
+      "title": "H A R D",
+      "message": "YoRHa No. 2 Type B",
+      "time": 3,
+      "lives": 2,
+      "display": "Hard",
+      "sound": document.getElementById("hard-voice"),
+    };
+  }
+}
