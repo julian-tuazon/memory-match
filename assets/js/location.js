4b01c0e3bc45549cde8934ff11f4d042a19f6030
Julian <jthrow00@gmail.com>
2020-04-06 03:18:24 -0700

    Created Location class with properties baseed on former location obj

--- assets/js/location.js
+++ assets/js/location.js
@@ -9,28 +9,28 @@ class Location {
     this.currentMessage = "Select Location";
     this.itemList = [
       "sol",
-      "moaTherma",
-      "vinetaK",
-      "techDeRa",
+      "moa",
+      "vineta",
+      "tech",
       "metropia",
-      "anulphaPass",
+      "anulpha",
     ];
     this.sol = {
       "title": "L O C A T I O N",
       "message": "Sol 2",
       "sound": document.getElementById("sol-2-voice"),
     };
-    this.moaTherma = {
+    this.moa = {
       "title": "L O C A T I O N",
       "message": "Moa Therma",
       "sound": document.getElementById("moa-therma-voice"),
     };
-    this.vinetaK = {
+    this.vineta = {
       "title": "L O C A T I O N",
       "message": "Vineta K",
       "sound": document.getElementById("vineta-k-voice"),
     };
-    this.techDeRa = {
+    this.tech = {
       "title": "L O C A T I O N",
       "message": "Tech De Ra",
       "sound": document.getElementById("tech-de-ra-voice"),
@@ -40,59 +40,10 @@ class Location {
       "message": "Metropia",
       "sound": document.getElementById("metropia-voice"),
     };
-    this.anulphaPass = {
+    this.anulpha = {
       "title": "L O C A T I O N",
       "message": "Anulpha Pass",
       "sound": document.getElementById("anulpha-pass-voice"),
     };
-
-
-    this.location = {
-      "name": "location",
-      "button": document.getElementById('location-button'),
-      "current": "null",
-      "defaultTitle": "L O C A T I O N",
-      "currentTitle": "L O C A T I O N",
-      "defaultMessage": "Select Location",
-      "currentMessage": "Select Location",
-      "itemList": [
-        "sol-2",
-        "moa-therma",
-        "vineta-k",
-        "tech-de-ra",
-        "metropia",
-        "anulpha-pass",
-      ],
-      "sol-2": {
-        "title": "L O C A T I O N",
-        "message": "Sol 2",
-        "sound": document.getElementById("sol-2-voice"),
-      },
-      "moa-therma": {
-        "title": "L O C A T I O N",
-        "message": "Moa Therma",
-        "sound": document.getElementById("moa-therma-voice"),
-      },
-      "vineta-k": {
-        "title": "L O C A T I O N",
-        "message": "Vineta K",
-        "sound": document.getElementById("vineta-k-voice"),
-      },
-      "tech-de-ra": {
-        "title": "L O C A T I O N",
-        "message": "Tech De Ra",
-        "sound": document.getElementById("tech-de-ra-voice"),
-      },
-      "metropia": {
-        "title": "L O C A T I O N",
-        "message": "Metropia",
-        "sound": document.getElementById("metropia-voice"),
-      },
-      "anulpha-pass": {
-        "title": "L O C A T I O N",
-        "message": "Anulpha Pass",
-        "sound": document.getElementById("anulpha-pass-voice"),
-      },
-    };
   }
-};
+}
