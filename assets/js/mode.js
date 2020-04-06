f796d89a328806bdc17459a7086905762ab895d5
Julian <jthrow00@gmail.com>
2020-04-06 03:18:47 -0700

    Created Mode class with properties baseed on former mode obj

--- assets/js/mode.js
+++ assets/js/mode.js
@@ -1,25 +1,27 @@
-const myMode = {
-  "name": "mode",
-  "button": document.getElementById('mode-button'),
-  "current": "null",
-  "defaultTitle": "M O D E",
-  "currentTitle": "M O D E",
-  "defaultMessage": "Deploy Pod",
-  "currentMessage": "Deploy Pod",
-  "itemList": [
-    "survival",
-    "time-attack",
-  ],
-  "survival": {
-    "title": "S U R V I V A L",
-    "message": "Pod X-042",
-    "display": "Survival",
-    "sound": document.getElementById("survival-voice"),
-  },
-  "time-attack": {
-    "title": "T I M E - A T T A C K",
-    "message": "Pod Y-153",
-    "display": "Time-Attack",
-    "sound": document.getElementById("time-attack-voice"),
-  },
-};
+class Mode {
+  constructor() {
+    this.name = "mode";
+    this.button = document.getElementById('mode-button');
+    this.current = "null";
+    this.defaultTitle = "M O D E";
+    this.currentTitle = "M O D E";
+    this.defaultMessage = "Deploy Pod";
+    this.currentMessage = "Deploy Pod";
+    this.itemList = [
+      "survival",
+      "timeattack",
+    ];
+    this.survival = {
+        title: "S U R V I V A L",
+        message: "Pod X-042",
+        display: "Survival",
+        sound: document.getElementById("survival-voice"),
+    };
+    this.timeattack = {
+        "title": "T I M E - A T T A C K",
+        "message": "Pod Y-153",
+        "display": "Time-Attack",
+        "sound": document.getElementById("time-attack-voice"),
+    };
+  }
+}
