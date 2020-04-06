c4766b085c593889bda24e4e1c3306b958fd11d5
Julian <jthrow00@gmail.com>
2020-04-06 02:26:57 -0700

    Added soundModal and welcomeModal properties to reference sound and welcome modals; added toggle property initially set to null in music, soundEffects, voice objects

--- assets/js/sound.js
+++ assets/js/sound.js
@@ -1,5 +1,8 @@
 class Sound {
   constructor() {
+    this.soundModal = document.getElementById("sound-modal");
+    this.welcomeModal = document.getElementById("welcome-modal");
+
     this.gameMusic = document.getElementById("bgm");
     this.startSound = document.getElementById("start-sound");
     this.endSound = document.getElementById("end-sound");
@@ -20,18 +23,21 @@ class Sound {
     this.music = {
       name: 'Music',
       button: this.musicButton,
+      toggle: null,
       itemList: [...document.getElementById('music').children],
     };
 
     this.soundEffects = {
       name: 'SFX',
       button: this.soundEffectsButton,
+      toggle: null,
       itemList: [...document.getElementById('sound-effects').children],
     };
 
     this.voice = {
       name: 'Voice',
       button: this.voiceButton,
+      toggle: null,
       itemList: [...document.getElementById('voice').children],
     };
 
@@ -50,8 +56,8 @@ class Sound {
 
   configureSound(event) {
     if (event.target.id === 'sound-on-button') sound.soundList.forEach(sound => sound.toggle());
-    soundModal.classList.add("hidden");
-    welcomeModal.classList.remove("hidden");
+    this.soundModal.classList.add("hidden");
+    this.welcomeModal.classList.remove("hidden");
   }
 
   toggleSound(sound) {
@@ -73,148 +79,3 @@ class Sound {
     });
   }
 }
-  // Array.prototype.forEach.call(document.getElementsByClassName('clickable'), elem => elem.addEventListener('mouseover', () => hoverSound.play()));
-
-// this.toggle = {
-//   music: toggleSound(music),
-//   soundEffects: toggleSound(soundEffects),
-//   voice: toggleSound(voice),
-// };
-
-// this.toggleMusic = toggleSound(music);
-// this.toggleSoundEffects = toggleSound(soundEffects);
-// this.toggleVoice = toggleSound(voice);
-
-
-// this.voiceArray = [
-//   "ag-voice",
-//   "assegai-voice",
-//   "goteki-voice",
-//   "auricom-voice",
-//   "icaras-voice",
-//   "piranha-voice",
-//   "harimau-voice",
-//   "qirex-voice",
-//   "triakis-voice",
-//   "easy-voice",
-//   "medium-voice",
-//   "hard-voice",
-//   "survival-voice",
-//   "time-attack-voice",
-//   "sol-2-voice",
-//   "moa-therma-voice",
-//   "vineta-k-voice",
-//   "tech-de-ra-voice",
-//   "metropia-voice",
-//   "anulpha-pass-voice",
-// ];
-
-// this.toggleMusic = (function () {
-//   let toggle = true;
-//   return function () {
-//     if (toggle) {
-//       playSound(onSound);
-//       music.volume = 0.1;
-//       music.muted = false;
-//       music.play();
-//       toggle = false;
-//       musicButton.textContent = "Music | ON";
-//     } else {
-//       playSound(offSound);
-//       music.muted = true;
-//       music.pause();
-//       toggle = true;
-//       musicButton.textContent = "Music | OFF";
-//     }
-//   }
-// })();
-
-// Current bug - offSound will not play because sounds will be muted
-// this.toggleSoundEffects = (function () {
-//   let toggle = true;
-//   return function () {
-//     if (toggle) {
-//       playSound(onSound);
-//       toggle = false;
-//       soundEffectsButton.textContent = "SFX | ON";
-//     } else {
-
-//       playSound(offSound);
-//       toggle = true;
-//       soundEffectsButton.textContent = "SFX | OFF";
-//     }
-//     for (let m = 0; m < soundEffectsArray.length; m++) {
-//       soundEffectsArray[m].muted = toggle;
-//     }
-//   }
-// })();
-
-// this.toggleVoice = (function () {
-//   let toggle = true;
-//   return function () {
-//     if (toggle) {
-//       playSound(onSound);
-//       toggle = false;
-//       voiceButton.textContent = "Voice | ON";
-//     } else {
-//       playSound(offSound);
-//       toggle = true;
-//       voiceButton.textContent = "Voice | OFF";
-//     }
-//     for (let p = 0; p < voiceArray.length; p++) {
-//       document.getElementById(`${voiceArray[p]}`).muted = toggle;
-//     }
-//   }
-// })();
-
-
-
-// this.togMusic = toggleSound('music');
-// this.togSoundEffects = toggleSound('sfx');
-// this.togVoice = toggleSound('voice')
-
-
-
-// musicButton.addEventListener('click', toggleMusic);
-// soundEffectsButton.addEventListener('click', toggleSoundEffects);
-// voiceButton.addEventListener('click', toggleVoice);
-
-
-// this.musicArray = [gameMusic];
-
-// this.soundEffectsArray = [
-//   startSound,
-//   endSound,
-//   flipSound,
-//   correctSound,
-//   incorrectSound,
-//   onSound,
-//   offSound,
-//   resetSound,
-//   cheatSound,
-//   hoverSound,
-//   selectSound,
-// ];
-
-// this.voiceArray = [
-//   document.getElementById("ag-voice"),
-//   document.getElementById("assegai-voice"),
-//   document.getElementById("goteki-voice"),
-//   document.getElementById("auricom-voice"),
-//   document.getElementById("icaras-voice"),
-//   document.getElementById("piranha-voice"),
-//   document.getElementById("harimau-voice"),
-//   document.getElementById("qirex-voice"),
-//   document.getElementById("triakis-voice"),
-//   document.getElementById("easy-voice"),
-//   document.getElementById("medium-voice"),
-//   document.getElementById("hard-voice"),
-//   document.getElementById("survival-voice"),
-//   document.getElementById("time-attack-voice"),
-//   document.getElementById("sol-2-voice"),
-//   document.getElementById("moa-therma-voice"),
-//   document.getElementById("vineta-k-voice"),
-//   document.getElementById("tech-de-ra-voice"),
-//   document.getElementById("metropia-voice"),
-//   document.getElementById("anulpha-pass-voice"),
-// ];
