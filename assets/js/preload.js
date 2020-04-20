983f889c1cff7ffc469f2e131c9e70cc07961778
Julian <jthrow00@gmail.com>
2020-04-20 06:09:54 -0700

    Reduced amount of initial preload images in Promise.all

--- assets/js/preload.js
+++ assets/js/preload.js
@@ -25,50 +25,11 @@ function preloadImagePromise(src) {
   });
 }
 
-// const prom1 = new Promise(resolve => {
-//   const img = new Image();
-//   img.src = "assets/images/buttons/button1.gif";
-//   img.onload = () => {
-//     console.log('Promise made', img.src);
-//     resolve(img.src);
-//   }
-// });
-// const prom2 = new Promise(resolve => {
-//     const img = new Image();
-//     img.src = "assets/images/buttons/button3.gif";
-//     img.onload = () => {
-//       console.log('Promise made', img.src);
-//       resolve(img.src);
-//     }
-// });
-
-  // preloadImage("assets/images/buttons/button3.gif"),
-  // preloadImage("assets/images/buttons/survival.png"),
-  // preloadImage("assets/images/buttons/time_attack.png"),
-  // preloadImage("assets/images/buttons/easy.png"),
-  // preloadImage("assets/images/buttons/medium.png"),
-  // preloadImage("assets/images/buttons/hard.png"),
-  // preloadImage("assets/images/buttons/anulpha_pass.jpg"),
-  // preloadImage("assets/images/buttons/metropia.jpg"),
-  // preloadImage("assets/images/buttons/moa_therma.jpg"),
-  // preloadImage("assets/images/buttons/sol2.jpg"),
-  // preloadImage("assets/images/buttons/tech_de_ra.jpg"),
-  // preloadImage("assets/images/buttons/vineta_k.jpg")
-
 Promise.all([
   preloadImagePromise("assets/images/buttons/button1.gif"),
   preloadImagePromise("assets/images/buttons/button3.gif"),
   preloadImagePromise("assets/images/buttons/survival.png"),
   preloadImagePromise("assets/images/buttons/time_attack.png"),
-  preloadImagePromise("assets/images/buttons/easy.png"),
-  preloadImagePromise("assets/images/buttons/medium.png"),
-  preloadImagePromise("assets/images/buttons/hard.png"),
-  preloadImagePromise("assets/images/buttons/anulpha_pass.jpg"),
-  preloadImagePromise("assets/images/buttons/metropia.jpg"),
-  preloadImagePromise("assets/images/buttons/moa_therma.jpg"),
-  preloadImagePromise("assets/images/buttons/sol2.jpg"),
-  preloadImagePromise("assets/images/buttons/tech_de_ra.jpg"),
-  preloadImagePromise("assets/images/buttons/vineta_k.jpg")
 ])
   .then((values) => {
     console.log('the deed is done!', 'values', values);
@@ -85,6 +46,15 @@ Promise.all([
 
 const loadGameImages = () => {
   preloadImages([
+    "assets/images/buttons/easy.png",
+    "assets/images/buttons/medium.png",
+    "assets/images/buttons/hard.png",
+    "assets/images/buttons/anulpha_pass.jpg",
+    "assets/images/buttons/metropia.jpg",
+    "assets/images/buttons/moa_therma.jpg",
+    "assets/images/buttons/sol2.jpg",
+    "assets/images/buttons/tech_de_ra.jpg",
+    "assets/images/buttons/vineta_k.jpg",
     "assets/images/backgrounds/anulpha_pass.jpg",
     "assets/images/backgrounds/metropia.jpg",
     "assets/images/backgrounds/moa_therma.jpg",
